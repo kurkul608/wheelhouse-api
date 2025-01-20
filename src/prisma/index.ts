d5edd4858b1c084ba3498fda1e9d7a74237de23b
Petr Molchanov <molc.pyotr@yandex.ru>
2025-01-19 21:24:19 -0300

    add dockerfile

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -3,4 +3,15 @@ import { PrismaClient } from "@prisma/client";
 console.log(process.env.DATABASE_URL);
 const prisma = new PrismaClient();
 
+(async () => {
+  try {
+    await prisma.$connect();
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+})();
+
 export default prisma;
